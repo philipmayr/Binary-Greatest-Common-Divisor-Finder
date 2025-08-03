f997dd8e6026e77724f27432d43c944e35379a76
Phil Samuel <philipmayr@outlook.com>
2025-08-03 16:10:36 +0200

    Update program.c

--- program.c
+++ program.c
@@ -35,43 +35,49 @@ int count_trailing_zeroes(int number)
     return trailing_zero_count - (number & 1);
 }
 
-int find_lesser_of_twain(int a, int b)
+int find_lesser_of_twain(int first_number, int second_number)
 {
-    return (a < b) ? a : b;
+    return (first_number < second_number) ? first_number : second_number;
 }
 
-int find_greatest_common_divisor(int a, int b)
+int get_distance_from_zero(int number)
 {
-    if (a == 0) return b;
-    if (b == 0) return a;
+    return (number < 0) ? -number : number;
+}
+
+int find_greatest_common_divisor(int first_number, int second_number)
+{
+    if (first_number == 0) return second_number;
+    if (second_number == 0) return first_number;
+        
+    int trailing_zero_count_of_first_number = count_trailing_zeroes(first_number);
+    int trailing_zero_count_of_second_number = count_trailing_zeroes(second_number);
     
-    int common_divisor = 1;
+    first_number >>= trailing_zero_count_of_first_number;
+    second_number >>= trailing_zero_count_of_second_number;
     
-    int trailing_zero_count_of_a = count_trailing_zeroes(a);
-    int trailing_zero_count_of_b = count_trailing_zeroes(b);
+    int common_binary_divisor = find_lesser_of_twain(trailing_zero_count_of_first_number, trailing_zero_count_of_second_number);
     
-    a >>= trailing_zero_count_of_a;
-    b >>= trailing_zero_count_of_b;
-    
-    common_divisor <<= find_lesser_of_twain(trailing_zero_count_of_a, trailing_zero_count_of_b);
-    
-    while (a)
+    while (first_number)
     {
-        if (a >= b) a = (a - b) >> 1;
-        else b = (b - a) >> 1;
+        int difference_between_numbers = first_number - second_number;
+        
+        second_number = find_lesser_of_twain(first_number, second_number);
+        first_number = get_distance_from_zero(difference_between_numbers);
+        first_number >>= count_trailing_zeroes(first_number);
     }
     
-    int greatest_common_divisor = common_divisor * b;
+    int greatest_common_divisor = second_number << common_binary_divisor;
     
     return greatest_common_divisor;
 }
 
 int main()
 {
-    int a = 37;
-    int b = 73;
+    int first_number = 16;
+    int second_number = 48;
 
-    printf("%d\n", find_greatest_common_divisor(a, b));
+    printf("%d\n", find_greatest_common_divisor(first_number, second_number));
     
     return 0;
 }
